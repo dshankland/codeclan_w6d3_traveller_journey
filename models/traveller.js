8d46240a645ee998131033ac01d3526b8b9dc81c
Darren Shankland <ds@4pq.co.uk>
2019-04-24 20:55:49 +0100

    whoops, forgot to commit more often

--- models/traveller.js
+++ models/traveller.js
@@ -3,27 +3,47 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-
+  const result = this.journeys.map((journey) => {
+    return journey.startLocation;
+  });
+  return result;
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
+  const result = this.journeys.map((journey) => {
+    return journey.endLocation;
+  });
+  return result;
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  const result = this.journeys.filter((journey) => {
+    return journey.transport === transport;
+  });
+  return result;
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  const result = this.journeys.filter((journey) => {
+    return journey.distance > minDistance;
+  });
+  return result;
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  const result = this.journeys.reduce((totalDistance, journey) => {
+    return totalDistance += journey.distance;
+  }, 0);
+  return result;
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
-
+  const uniqueModesOfTransport = []
+  const result = this.journeys.forEach((journey) => {
+    if (!uniqueModesOfTransport.includes(journey.transport)) uniqueModesOfTransport.push(journey.transport);
+  });
+  console.log('unique ', uniqueModesOfTransport);
+  return uniqueModesOfTransport;
 };
 
 
